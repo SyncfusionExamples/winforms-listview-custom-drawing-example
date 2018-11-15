26245f93e50e720075d4a2d5626816c6e86e2425
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2018-11-14 21:10:58 -0800

    Custom sample for SfListView multiline drawing

---
+++ MultiLine/CS/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace GettingStarted
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfListView1 = new Syncfusion.WinForms.ListView.SfListView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sfListView1
+            // 
+            this.sfListView1.AccessibleName = "ScrollControl";
+            this.sfListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sfListView1.Location = new System.Drawing.Point(0, 0);
+            this.sfListView1.Name = "sfListView1";
+            this.sfListView1.Size = new System.Drawing.Size(337, 315);
+            this.sfListView1.TabIndex = 0;
+            this.sfListView1.Text = "sfListView1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.sfListView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(337, 315);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 319);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Getting Started";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.ListView.SfListView sfListView1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
